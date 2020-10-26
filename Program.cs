9f6acac6840e0db67fcdb43a15eeae4ffd0c3825
oksogi <73480477+oksogi@users.noreply.github.com>
2020-10-26 18:29:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Program1
+{
+    class Program
+    {
+
+        private const string __NamesFile = "Strings1.txt";
+        static void Main(string[] args)
+        {
+            
+            using (StreamReader names_reader = new StreamReader(__NamesFile))
+            {
+                while (!names_reader.EndOfStream)
+                {
+                    var line = names_reader.ReadLine();
+
+                    string[] words = line.Split(new string[] { " ", "\r\n", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+                    string maxl = "";
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        //Console.WriteLine(words[i].Length);
+                        if (maxl.Length < words[i].Length) maxl = words[i];
+                    }
+                    //Console.OutputEncoding = Encoding.UTF8;
+                    Console.WriteLine("Самое длинное слово: ");
+                    Console.WriteLine(maxl);
+                    //Console.WriteLine(line);
+                }
+                
+                //names_reader.Close();
+                //names_reader.Dispose();
+            }
+           
+            
+
+            Console.WriteLine("Нажмите для выхода");
+            Console.ReadLine();
+
+        }
+    }
+}
+
